21b4c1ae69875b329f61f3a7a1ce86e502ad1c47
IoTKETI <iot.keti@gmail.com>
2020-11-23 15:20:49 +0900

    - edit topic for test 2

--- lib_sparrow_gun.py
+++ lib_sparrow_gun.py
@@ -11,7 +11,8 @@ argv = sys.argv
 
 global lib
 global lib_topic
-broker_ip = 'localhost'
+# broker_ip = 'localhost'
+broker_ip = '203.253,128.177'
 port = 1883
 
 
@@ -67,7 +68,8 @@ def msw_mqtt_connect(broker_ip, port):
     lib_mqtt_client.on_message = on_message
     lib_mqtt_client.connect(broker_ip, port)
     container_name = lib["control"][0]
-    control_topic = '/MUV/control/' + lib["name"] + '/' + container_name
+#     control_topic = '/MUV/control/' + lib["name"] + '/' + container_name
+    control_topic = '/Mobius/KETI_MUV/Mission_Data/Gun_Tester/msw_sparrow_gun/MICRO'
     lib_mqtt_client.subscribe(control_topic, 0)
     print(control_topic)
     lib_mqtt_client.loop_start()
