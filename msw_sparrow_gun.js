074592512b68e0c5523f0adda7a84f7960026a50
dnjstjr93 <dnjstjr93@gmail.com>
2022-10-19 00:21:59 +0900

    - 기본 포트 변경

--- msw_sparrow_gun.js
+++ msw_sparrow_gun.js
@@ -29,7 +29,7 @@ global.drone_info = '';
 config.name = 'msw_sparrow_gun';
 
 try {
-    drone_info = JSON.parse(fs.readFileSync('./drone_info.json', 'utf8'));
+    drone_info = JSON.parse(fs.readFileSync('../drone_info.json', 'utf8'));
 
     config.directory_name = config.name + '_' + config.name;
     // config.sortie_name = '/' + sortie_name;
